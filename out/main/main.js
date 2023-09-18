b228d28d71786b3cd816b34ad56b68807a548646
Thomson <thomsonbingo@gmail.com>
2023-09-19 01:01:14 +0530

    Electron, TS, Vite Scaffold

---
+++ out/main/main.js
@@ -0,0 +1,30 @@
+"use strict";
+const electron = require("electron");
+require("path");
+let mainWindow;
+function createWindow() {
+  mainWindow = new electron.BrowserWindow({
+    frame: false,
+    transparent: true,
+    width: 300,
+    height: 300,
+    webPreferences: {
+      devTools: true
+    }
+  });
+  mainWindow.loadURL("http://localhost:5173");
+  mainWindow.on("closed", () => mainWindow = null);
+}
+electron.app.whenReady().then(() => {
+  createWindow();
+});
+electron.app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    electron.app.quit();
+  }
+});
+electron.app.on("activate", () => {
+  if (mainWindow == null) {
+    createWindow();
+  }
+});
---
+++ src/main/main.js
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    frame: false,
+    transparent: true,
+    width: 300,
+    height: 300,
+    webPreferences: {
+      devTools: true
+    }
+  });
+
+  // Vite dev server URL
+  mainWindow.loadURL('http://localhost:5173');
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow == null) {
+    createWindow();
+  }
+});
