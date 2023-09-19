488a80f715f350c713ac0f6ec93ae22dcbc19eef
Thomson <thomsonbingo@gmail.com>
2023-09-19 20:17:47 +0530

    Custom window and styled components setup

---
+++ src/renderer/src/App.styled.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const AppWrapper = styled.div`
+background: grey;
+border-radius: 50%;
+height: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+-webkit-app-region: drag;
+`
+
+export const CloseBtn = styled.div`
+cursor: pointer;
+background: red;
+display: flex;
+justify-content: center;
+align-items: center;
+width: 20px;
+height: 20px;
+border-radius: 6px;
+position: absolute;
+top: 0;
+right: 10%;
+`
